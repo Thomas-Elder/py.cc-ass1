2b57a22f3411c4ef14cd164cce30d885ea309261
Thomas <thombsaway@gmail.com>
2021-06-25 12:48:19 +1000

    Wire in datastore

--- db/db.py
+++ db/db.py
@@ -1,4 +1,7 @@
 
+from google.cloud import datastore
+from pyasn1.type.univ import Null
+
 from .models.post import Post
 from .models.user import User
 
@@ -9,15 +12,45 @@ class DB:
     #
     #
     #
-    def __init__(self, datastore_client) -> None:
-        # add test creds
-        self.users[1] = User(1, 'tom', '1234')
+    def __init__(self):
+
+        self.client = datastore.Client()
+
+        key = self.client.key('users', '1')
+        user = datastore.Entity(key=key)
+        user.update(
+            {
+                'id': '1', 
+                'username':'tom', 
+                'password':'1234'
+            }
+            )
+
+        self.client.put(user)
+
     #
+    #
+    #
+    def getusers(self):
+        query = self.client.query(kind='users')
+        users = query.fetch()
+
+        return users
+
+    #   
     #
     #
     def getuser(self, id):
 
-        return self.users[id]
+        key = self.client.key('users', id)
+        result = self.client.get(key)
+
+        if result == None:
+            return None
+
+        else:
+            user = User(result['id'], result['username'], result['password'])
+            return user
 
     #
     #
@@ -39,8 +72,13 @@ class DB:
     #
     def checkpassword(self, id, password) -> bool:
 
-        if id in self.users:
-            return self.users[id].password == password
+        key = self.client.key('users', id)
+        result = self.client.get(key)
+
+        if result != Null and result['password'] == password:
+
+            return User(result['id'], result['username'], result['password'])
+
         else:
             return False
 
