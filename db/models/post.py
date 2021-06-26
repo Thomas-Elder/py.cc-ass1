e933c403be7138b340db55a2ad5f93a0628f8c39
Thomas <thombsaway@gmail.com>
2021-06-27 09:59:45 +1000

    add fields to post.

--- db/models/post.py
+++ db/models/post.py
@@ -2,12 +2,13 @@ from datetime import datetime
 
 class Post():
 
-    def __init__(self, subject, message, user):
-        #self.id = id
+    def __init__(self, subject, message, user, id=None, datetime=None):
+        
         self.subject = subject
         self.message = message
         self.user = user
-        self.datetime = datetime.now()
+        self.datetime = datetime
+        self.id = id
 
     def editmessage(self, message):
         self.message = message
