7d045308df67ee41b14777694e35a6c272b603e4
Thomas <thombsaway@gmail.com>
2021-06-27 09:59:57 +1000

    add validate function to login form to check against db

--- forms/login.py
+++ forms/login.py
@@ -1,8 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+from db.db import DB
 
 class LoginForm(FlaskForm):
     id = StringField('ID', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Log In')
+
+    def validate(self):
+
+        if not FlaskForm.validate(self):
+            return False
+        else:
+            db = DB()
+
+            if db.checkpassword(self.id.data, self.password.data):
+                self.password.errors.append('ID or password is invalid')
+                return True
+
+            return False
