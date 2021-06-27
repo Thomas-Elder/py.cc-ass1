bf67780fd68d6af4350daed1db012ea3735ce466
Thomas <thombsaway@gmail.com>
2021-06-27 19:46:03 +1000

    Add avatar field to registration.

--- forms/register.py
+++ forms/register.py
@@ -1,12 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, ValidationError
+from flask_wtf.file import FileField, FileRequired
+from wtforms.validators import DataRequired
 from db.db import DB
 
 class RegisterForm(FlaskForm):
     id = StringField('ID', validators=[DataRequired()])
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    avatar = FileField('Avatar', validators=[FileRequired()])
     submit = SubmitField('Register')
 
     def validate(self):
