29049aa84c96032cc3ba7c0a71e305a1dc3efe0d
Thomas <thombsaway@gmail.com>
2021-06-28 21:12:02 +1000

    add explicit validation call to debug

--- forms/updatepost.py
+++ forms/updatepost.py
@@ -1,10 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, HiddenField
 from flask_wtf.file import FileField, FileRequired
 from wtforms.validators import DataRequired
 
 class UpdatePostForm(FlaskForm):
+    id = HiddenField('', validators=[DataRequired()])
     subject = StringField('Subject', validators=[DataRequired()])
     message = StringField('Message', validators=[DataRequired()])
     image = FileField('Image', validators=[FileRequired()])
     submit = SubmitField('Update')
+
+    def validate(self):
+
+        if not FlaskForm.validate(self):
+            print('why are we here?')
+            return False
+
+        else:
+            return True
