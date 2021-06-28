c766309ff068c5127551516a13e325133cd68edf
Thomas <thombsaway@gmail.com>
2021-06-28 12:42:02 +1000

    Set up updatepassword form.

---
+++ forms/updatepassword.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+from db.db import DB
+
+class UpdatePasswordForm(FlaskForm):
+    id = StringField('ID', validators=[DataRequired()])
+    oldpassword = PasswordField('Old Password', validators=[DataRequired()])
+    newpassword = PasswordField('New Password', validators=[DataRequired()])
+    submit = SubmitField('Change')
+
+    def validate(self):
+
+        if not FlaskForm.validate(self):
+            return False
+        else:
+            db = DB()
+
+            if db.checkpassword(self.id.data, self.oldpassword.data):
+                self.oldpassword.errors.append('The old password is incorrect')
+                return True
+
+            return False
