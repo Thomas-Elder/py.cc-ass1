38d51efb07b7c9a0e4cdb28f6f192fd7f5d881c4
Thomas <thombsaway@gmail.com>
2021-06-28 12:33:14 +1000

    update post form to have image field.

--- forms/post.py
+++ forms/post.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from flask_wtf.file import FileField, FileRequired
 from wtforms.validators import DataRequired
 
 class PostForm(FlaskForm):
     subject = StringField('Subject', validators=[DataRequired()])
     message = StringField('Message', validators=[DataRequired()])
-    image = PasswordField('Image')
+    image = FileField('Image', validators=[FileRequired()])
     submit = SubmitField('Post')
