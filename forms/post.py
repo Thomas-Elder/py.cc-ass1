527fe7ea9f24acbe579d0f0b7ffb406efcf8ed15
Thomas <thombsaway@gmail.com>
2021-06-30 19:48:39 +1000

    Update message to textareafield.

--- forms/post.py
+++ forms/post.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField
 from flask_wtf.file import FileField, FileRequired
 from wtforms.validators import DataRequired
 
 class PostForm(FlaskForm):
     subject = StringField('Subject', validators=[DataRequired()])
-    message = StringField('Message', validators=[DataRequired()])
+    message = TextAreaField('Message', validators=[DataRequired()])
     image = FileField('Image', validators=[FileRequired()])
     submit = SubmitField('Post')
